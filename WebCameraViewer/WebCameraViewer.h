734b41ee18a26d09532819675ad7c1d118460d6a
galaxy-wizard <kozlov_sergey_aleksandrovitch@mail.ru>
2019-04-23 20:02:05 +0300

    Add project files.

---
+++ WebCameraViewer/WebCameraViewer.h
@@ -0,0 +1,32 @@
+﻿
+// WebCameraViewer.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CWebCameraViewerApp:
+// See WebCameraViewer.cpp for the implementation of this class
+//
+
+class CWebCameraViewerApp : public CWinApp
+{
+public:
+	CWebCameraViewerApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CWebCameraViewerApp theApp;
---
+++ WebCameraViewer/WebCameraViewerDlg.h
@@ -0,0 +1,33 @@
+﻿
+// WebCameraViewerDlg.h : header file
+//
+
+#pragma once
+
+
+// CWebCameraViewerDlg dialog
+class CWebCameraViewerDlg : public CDialogEx
+{
+// Construction
+public:
+	CWebCameraViewerDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_WEBCAMERAVIEWER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
